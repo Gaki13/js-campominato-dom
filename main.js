3aa2871f69d7ddf67d9bb6807690c8a3c011fc71
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-09 17:43:14 +0200

    chiedo all'utente un numero compreso tra 1 e 100 finché non perde

--- main.js
+++ main.js
@@ -18,19 +18,32 @@ console.log(bombsList);
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 const userNumbers = [];
 
-while (userNumbers.length < 10){
+while (userNumbers.length < 9){
     let num = Number(prompt("Inserrisci un numero tra 1 e 100:"))
     
     // L’utente non può inserire più volte lo stesso numero.
     if(userNumbers.includes(num)){
         alert("inserisci un numero diverso")
-    }else{
+    }else if( num > 100 ){
+        //Controllo che i numeri inseriti vadano da 1 a 100
+        alert ("inserisci un numero COMPRESO TRA 1 e 100")
+    }else if( num < 100){
       userNumbers.push(num);  
+    };
+    
+    // Se il numero è presente nella lista dei numeri generati, la partita termina
+    if (bombsList.includes(num)){
+        alert("hai perso")
+        bombsList.length = 0   
+     // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+        console.log(userNumbers.length);
+        userNumbers.length = 0
     };
 }
 
 console.log (userNumbers);
 
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+//altrimenti si continua chiedendo all’utente un altro numero.
+
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
