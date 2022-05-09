49155f74bebb8587aa390137ed3b3b2a41ab64ad
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-09 17:14:54 +0200

    chiedo all'utente un numero e verifico che non sia stato inserito prima

--- main.js
+++ main.js
@@ -16,7 +16,21 @@ while(count !== 16){
 console.log(bombsList);
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
+const userNumbers = [];
+
+while (userNumbers.length < 10){
+    let num = Number(prompt("Inserrisci un numero tra 1 e 100:"))
+    
+    // L’utente non può inserire più volte lo stesso numero.
+    if(userNumbers.includes(num)){
+        alert("inserisci un numero diverso")
+    }else{
+      userNumbers.push(num);  
+    };
+}
+
+console.log (userNumbers);
+
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
